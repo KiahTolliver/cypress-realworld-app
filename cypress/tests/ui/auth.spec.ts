bb29b12dde8236849418e3dd3601595804881913
Kiah Tolliver <kiah@c-trax.com>
2022-02-10 23:56:39 -0500

    Update auth.spec.ts

--- cypress/tests/ui/auth.spec.ts
+++ cypress/tests/ui/auth.spec.ts
@@ -108,49 +108,34 @@ describe("User Sign-up and Login", function () {
     cy.visualSnapshot("Redirect to SignIn");
   });
 
-  it("should display login errors", function () {
-    cy.visit("/");
 
-    cy.getBySel("signin-username").type("User").find("input").clear().blur();
-    cy.get("#username-helper-text").should("be.visible").and("contain", "Username is required");
-    cy.visualSnapshot("Display Username is Required Error");
 
-    cy.getBySel("signin-password").type("abc").find("input").blur();
-    cy.get("#password-helper-text")
-      .should("be.visible")
-      .and("contain", "Password must contain at least 4 characters");
-    cy.visualSnapshot("Display Password Error");
+  // it("should display signup errors", function () {
+  //   cy.intercept("GET", "/signup");
 
-    cy.getBySel("signin-submit").should("be.disabled");
-    cy.visualSnapshot("Sign In Submit Disabled");
-  });
+  //   cy.visit("/signup");
 
-  it("should display signup errors", function () {
-    cy.intercept("GET", "/signup");
+  //   cy.getBySel("signup-first-name").type("First").find("input").clear().blur();
+  //   cy.get("#firstName-helper-text").should("be.visible").and("contain", "First Name is required");
 
-    cy.visit("/signup");
+  //   cy.getBySel("signup-last-name").type("Last").find("input").clear().blur();
+  //   cy.get("#lastName-helper-text").should("be.visible").and("contain", "Last Name is required");
 
-    cy.getBySel("signup-first-name").type("First").find("input").clear().blur();
-    cy.get("#firstName-helper-text").should("be.visible").and("contain", "First Name is required");
+  //   cy.getBySel("signup-username").type("User").find("input").clear().blur();
+  //   cy.get("#username-helper-text").should("be.visible").and("contain", "Username is required");
 
-    cy.getBySel("signup-last-name").type("Last").find("input").clear().blur();
-    cy.get("#lastName-helper-text").should("be.visible").and("contain", "Last Name is required");
+  //   cy.getBySel("signup-password").type("password").find("input").clear().blur();
+  //   cy.get("#password-helper-text").should("be.visible").and("contain", "Enter your password");
 
-    cy.getBySel("signup-username").type("User").find("input").clear().blur();
-    cy.get("#username-helper-text").should("be.visible").and("contain", "Username is required");
+  //   cy.getBySel("signup-confirmPassword").type("DIFFERENT PASSWORD").find("input").blur();
+  //   cy.get("#confirmPassword-helper-text")
+  //     .should("be.visible")
+  //     .and("contain", "Password does not match");
+  //   cy.visualSnapshot("Display Sign Up Required Errors");
 
-    cy.getBySel("signup-password").type("password").find("input").clear().blur();
-    cy.get("#password-helper-text").should("be.visible").and("contain", "Enter your password");
-
-    cy.getBySel("signup-confirmPassword").type("DIFFERENT PASSWORD").find("input").blur();
-    cy.get("#confirmPassword-helper-text")
-      .should("be.visible")
-      .and("contain", "Password does not match");
-    cy.visualSnapshot("Display Sign Up Required Errors");
-
-    cy.getBySel("signup-submit").should("be.disabled");
-    cy.visualSnapshot("Sign Up Submit Disabled");
-  });
+  //   cy.getBySel("signup-submit").should("be.disabled");
+  //   cy.visualSnapshot("Sign Up Submit Disabled");
+  // });
 
   it("should error for an invalid user", function () {
     cy.login("invalidUserName", "invalidPa$$word");
